0b581109b7fe1651bf3ef06edc842b25b41199b0
Sarmaad Amin <sarmaad@gmail.com>
2024-09-09 00:51:59 +1000

    minor backend changes

--- node/src/index.ts
+++ node/src/index.ts
@@ -21,7 +21,7 @@ import { ErrorRegistry, BaseError } from 'new-error';
 dotenv.config();
 
 const app: Express = express();
-app.use(express.json());
+app.use(express.json({limit:'50mb'}));
 app.use(cors());
 
 const port = process.env.PORT || 3000;
